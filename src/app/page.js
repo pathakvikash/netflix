9132d6d0872be2eb6979baad9552d17e137900d6
Vikash Pathak <pathakvikash821@gmail.com>
2024-07-02 00:27:10 +0530

    feat: Layout & Homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,33 @@
-import Image from "next/image";
-
+import HomePage from '@/components/HomePage';
+import Footer from '@/components/Footer';
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+  let data = [
+    {
+      id: 1022789,
+      title: 'Inside Out 2',
+      imgsrc:
+        'https://assets.nflxext.com/ffe/siteui/vlv3/a56dc29b-a0ec-4f6f-85fb-50df0680f80f/2f8ae902-8efe-49bb-9a91-51b6fcc8bf46/IN-en-20240617-popsignuptwoweeks-perspective_alpha_website_small.jpg',
+      rating: 7.745,
+      overview:
+        "Teenager Riley's mind headquarters is undergoing a sudden demolition to make room for something entirely unexpected: new Emotions! Joy, Sadness, Anger, Fear and Disgust, who’ve long been running a successful operation by all accounts, aren’t sure how to feel when Anxiety shows up. And it looks like she’s not alone.",
+      poster: 'https://image.tmdb.org/t/p/w500/oxxqiyWrnM0XPnBtVe9TgYWnPxT.jpg',
+      banner:
+        'https://image.tmdb.org/t/p/original/stKGOm8UyhuLPR9sZLjs5AkmncA.jpg',
+      genre: [
+        {
+          id: 12,
+          name: 'Adventure',
+        },
+        {
+          id: 16,
+          name: 'Animation',
+        },
+        {
+          id: 35,
+          name: 'Comedy',
+        },
+      ],
+    },
+  ];
+  return <HomePage data={data} />;
 }
---
+++ src/app/signin/page.js
@@ -0,0 +1,79 @@
+'use client';
+import React, { useState } from 'react';
+const Signin = () => {
+  return (
+    <div className='bg-black text-white flex flex-col justify-center items-center '>
+      <img
+        className='w-full h-[60%] opacity-50 border border-gray-600 border-b-[6px]  bg-cover overflow-hidden object-cover'
+        src='https://assets.nflxext.com/ffe/siteui/vlv3/a56dc29b-a0ec-4f6f-85fb-50df0680f80f/2f8ae902-8efe-49bb-9a91-51b6fcc8bf46/IN-en-20240617-popsignuptwoweeks-perspective_alpha_website_small.jpg'
+        alt=''
+      />
+      <div className='absolute top-40 '>
+        <SignInForm />
+      </div>
+    </div>
+  );
+};
+
+export default Signin;
+
+function SignInForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    if (email && password) {
+      localStorage.setItem('login', 'true');
+      window.location.href = '/';
+    }
+  };
+  return (
+    <div className=' bg-black p-4 w-[400px] h-[70vh]'>
+      <div className='max-w-md mx-auto p-7'>
+        <h2 className='text-2xl font-bold text-white mb-4'>Sign In</h2>
+        <input
+          type='email'
+          className='w-full bg-transparent p-2 border border-gray-600 rounded mb-2'
+          placeholder='Email or phone number'
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+        <input
+          type='password'
+          className='w-full p-2 border bg-transparent border-gray-600 rounded mb-2'
+          placeholder='Password'
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+        <button
+          onClick={handleSubmit}
+          className='w-full bg-red-600 text-white py-2 rounded hover:bg-red-800'
+        >
+          Sign In
+        </button>
+        <p className='text-sm flex justify-center text-gray-400 mt-2'>OR</p>
+        <button className='w-full bg-gray-600 text-white py-2 rounded hover:bg-gray-700 mb-2'>
+          Use a Sign-In Code
+        </button>
+        <button className='w-full  text-white py-2 rounded hover:bg-gray-700'>
+          Forgot Password ?
+        </button>
+        <div className='flex items-center justify-start'>
+          <input type='checkbox' defaultChecked className='mr-2' />
+          <label className='text-gray-400'>Remember me</label>
+        </div>
+        <div className='mt-4 flex items-center text-center'>
+          <p className='text-sm text-gray-400'>New to Netflix?</p>
+          <button className=' text-white py-2 px-2'>Sign up now</button>
+        </div>
+        <p className='text-xs text-gray-400 mt-2'>
+          This page is protected by Google reCAPTCHA to ensure you're not a bot.
+          <a className='underline text-blue-700 px-2' href='#'>
+            Learn more
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+}
